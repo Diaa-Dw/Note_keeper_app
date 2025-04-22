307280c6a57fb4667eb37b580b814ef819a057c1
Diaa-dw <oava9944@gmail.com>
2025-04-22 13:35:40 +0300

    Install tanstack query and setup in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,17 @@ import App from "./App.tsx";
 import "./index.css";
 import { AppThemeProvider } from "./theme";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AppThemeProvider>
       <BrowserRouter>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </BrowserRouter>
     </AppThemeProvider>
   </StrictMode>
