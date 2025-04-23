fab79f8fbfdcec61ed90fb8c4bfddb5138b9bf9d
Diaa-Dw <oava9944@gmail.com>
2025-04-23 11:03:33 +0300

    Implement login API function with error handling and local storage management

---
+++ src/pages/Login/api/login.ts
@@ -0,0 +1,57 @@
+import axios from "axios";
+import { LoginFormData } from "../Login.type";
+import Cookies from "js-cookie";
+
+const API_URL = "https://fts-note-keeper-2.onrender.com/api/v1/users/login";
+
+const login = async ({ email, password }: LoginFormData) => {
+  // Check if email and password are provided
+  console.log("🚀 ~ login ~ email:", email);
+  if (!email || !password) {
+    throw new Error("Email and password are required.");
+  }
+
+  try {
+    const res = await axios.post(
+      API_URL,
+      {
+        email: email,
+        password: password,
+      },
+      {
+        withCredentials: true,
+      }
+    );
+
+    const data = res.data.data;
+    //store the user data in local storage
+    if (data) {
+      const user = {
+        id: data.user._id,
+        username: data.user.username,
+        profile: data.user?.photo || "",
+      };
+      localStorage.setItem("user", JSON.stringify(user));
+      Cookies.set("token", data.token, {
+        expires: 7,
+        sameSite: "Lax",
+        secure: true,
+      });
+    }
+
+    return data;
+  } catch (error: unknown) {
+    console.log("🚀 ~ login ~ error:", error);
+    if (axios.isAxiosError(error)) {
+      const message =
+        error?.response?.data?.message ||
+        error?.message ||
+        "Login failed. Please check your credentials.";
+      throw new Error(message);
+    } else {
+      throw new Error("An unexpected error occurred.");
+    }
+  }
+};
+
+export default login;
