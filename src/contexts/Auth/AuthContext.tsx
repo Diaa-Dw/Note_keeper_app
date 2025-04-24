924a6f6fa88bcabfe13b05e29cf4722cad16bf78
Diaa-Dw <oava9944@gmail.com>
2025-04-24 21:13:30 +0300

    Add useLoadAuth hook to manage authentication state loading

--- src/contexts/Auth/AuthContext.tsx
+++ src/contexts/Auth/AuthContext.tsx
@@ -1,6 +1,7 @@
 import { useReducer } from "react";
 import { AuthDispatchContext, AuthStateContext } from "./useAuth";
 import authReducer from "./authReducer";
+import useLoadAuth from "../../hooks/useLoadAuth";
 
 const initialState = {
   user: null,
@@ -10,6 +11,9 @@ const initialState = {
 
 export const AuthProvider = ({ children }: WithChildren) => {
   const [state, dispatch] = useReducer(authReducer, initialState);
+
+  //check if user loged in and if then load data to auth-context
+  useLoadAuth(dispatch);
 
   return (
     <AuthStateContext.Provider value={state}>
