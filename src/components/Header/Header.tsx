7e6ef30603975a38fb0e6dc298579d7472cd03c9
Diaa-Dw <oava9944@gmail.com>
2025-04-26 19:40:14 +0300

    Refactor Header component: enhance structure by adding dropdown menu for user actions and implement logout handling

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,30 @@
-import { Avatar, Typography } from "@mui/joy";
+import {
+  Avatar,
+  Dropdown,
+  Menu,
+  MenuButton,
+  MenuItem,
+  Typography,
+} from "@mui/joy";
 import { AvatarBox, LogoLink, StyledHeader } from "./Header.style";
 import ImportContactsRoundedIcon from "@mui/icons-material/ImportContactsRounded";
-import { useAuthState } from "../../contexts/Auth/useAuth";
+import { useAuthDispatch, useAuthState } from "../../contexts/Auth/useAuth";
+import { AccountCircle, DarkMode, Logout } from "@mui/icons-material";
+import Cookies from "js-cookie";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
   const { user, isAuthenticated } = useAuthState();
+  const dispatch = useAuthDispatch();
+  const navigate = useNavigate();
+
+  const onLogout = () => {
+    localStorage.removeItem("user");
+    Cookies.remove("jwt");
+    navigate("/login");
+    dispatch({ type: "LOGOUT" });
+  };
+
   const profileImage = user?.photo;
   return (
     <StyledHeader component={"header"}>
@@ -14,19 +34,38 @@ const Header = () => {
           NoteKeeper
         </Typography>
       </LogoLink>
-      <AvatarBox>
-        {isAuthenticated && (
-          <Avatar
-            src={profileImage}
-            alt={user?.username}
-            variant='solid'
-            size='lg'
-          />
-        )}
-        <Typography level='h3' component={"h3"}>
-          {user?.username}
-        </Typography>
-      </AvatarBox>
+
+      <Dropdown>
+        <MenuButton variant='plain'>
+          <AvatarBox>
+            {isAuthenticated && (
+              <Avatar
+                src={profileImage}
+                alt={user?.username}
+                variant='solid'
+                size='lg'
+              />
+            )}
+            <Typography level='h3' component={"h3"}>
+              {user?.username}
+            </Typography>
+          </AvatarBox>
+        </MenuButton>
+
+        <Menu>
+          <MenuItem>
+            <AccountCircle />
+            Profile
+          </MenuItem>
+          <MenuItem>
+            <DarkMode /> Theme
+          </MenuItem>
+          <MenuItem color='danger' onClick={onLogout}>
+            <Logout />
+            Logout
+          </MenuItem>
+        </Menu>
+      </Dropdown>
     </StyledHeader>
   );
 };
