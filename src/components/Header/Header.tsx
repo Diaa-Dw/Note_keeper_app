b813d88a2379cedf4faa83ce288ff98ab33d81e9
Diaa-Dw <oava9944@gmail.com>
2025-04-27 22:56:16 +0300

    Refactor Header component: remove unused imports and replace Dropdown with UserMenu for user profile display

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,37 +1,12 @@
-import {
-  Avatar,
-  Dropdown,
-  Menu,
-  MenuButton,
-  MenuItem,
-  Typography,
-  useColorScheme,
-} from "@mui/joy";
-import { AvatarBox, LogoLink, StyledHeader } from "./Header.style";
 import ImportContactsRoundedIcon from "@mui/icons-material/ImportContactsRounded";
-import { useAuthDispatch, useAuthState } from "../../contexts/Auth/useAuth";
-import { AccountCircle, DarkMode, Logout, Sunny } from "@mui/icons-material";
-import Cookies from "js-cookie";
-import { useNavigate } from "react-router-dom";
+import { Typography } from "@mui/joy";
+import { useAuthState } from "../../contexts/Auth/useAuth";
+import UserMenu from "../UserMenu";
+import { LogoLink, StyledHeader } from "./Header.style";
 
 const Header = () => {
-  const { mode, setMode } = useColorScheme();
   const { user, isAuthenticated } = useAuthState();
-  const dispatch = useAuthDispatch();
-  const navigate = useNavigate();
 
-  const onLogout = () => {
-    localStorage.removeItem("user");
-    Cookies.remove("jwt");
-    navigate("/login");
-    dispatch({ type: "LOGOUT" });
-  };
-
-  const handleThemeToggle = () => {
-    setMode(mode === "light" ? "dark" : "light");
-  };
-
-  const profileImage = user?.photo;
   return (
     <StyledHeader component={"header"}>
       <LogoLink to={"/"}>
@@ -40,39 +15,7 @@ const Header = () => {
           NoteKeeper
         </Typography>
       </LogoLink>
-
-      <Dropdown>
-        <MenuButton variant='plain'>
-          <AvatarBox>
-            {isAuthenticated && (
-              <Avatar
-                src={profileImage}
-                alt={user?.username}
-                variant='solid'
-                size='lg'
-              />
-            )}
-            <Typography level='h3' component={"h3"}>
-              {user?.username}
-            </Typography>
-          </AvatarBox>
-        </MenuButton>
-
-        <Menu>
-          <MenuItem>
-            <AccountCircle />
-            Profile
-          </MenuItem>
-          <MenuItem onClick={handleThemeToggle}>
-            {mode === "light" ? <DarkMode /> : <Sunny />}
-            Theme
-          </MenuItem>
-          <MenuItem color='danger' onClick={onLogout}>
-            <Logout />
-            Logout
-          </MenuItem>
-        </Menu>
-      </Dropdown>
+      {isAuthenticated && user && <UserMenu username={user?.username} />}
     </StyledHeader>
   );
 };
